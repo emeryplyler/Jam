c0425e191c1066b8751b144160879671f575925c
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-02-06 16:05:31 -0800

    created npc scene

--- world/house/door/door.gd
+++ world/house/door/door.gd
@@ -12,7 +12,6 @@ var ui: Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ui = get_tree().root.get_node("Root/UI/E")
-	print(ui)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,14 +22,13 @@ func _process(delta: float) -> void:
 func on_interact():
 	var target = destination.global_position
 	return(target)
-	
+
+
 func highlight():
 	ui.set_visible(true)
 	ui.get_node("OpenDoor").set_visible(true)
-	print("You're looking at", self)
 	
 
 func unhighlight():
-	print("You stopped looking at", self)
 	ui.set_visible(false)
 	ui.get_node("OpenDoor").set_visible(false)
