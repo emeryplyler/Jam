69309a8163eca1085fb2a2541712133044c8814d
emeryplyler <63931507+emeryplyler@users.noreply.github.com>
2025-03-01 09:20:40 -0800

    ribberette death fix

--- butterfly_rock.gd
+++ butterfly_rock.gd
@@ -1,6 +1,8 @@
 extends Sprite3D
 
-
+@export var deathsprite: Texture2D
+@export var ribberette: CharacterBody3D
+var gone = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +10,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not gone:
+		var ribberette_texture = ribberette.get_node("Sprite3D").texture
+		if ribberette_texture == ribberette.deathsprite and ribberette.visible:
+		#if ribberette.gone:
+			#ribberette.get_node("Sprite3D").set_modulate(Color(1, 1, 1, 0))
+			#set_texture(deathsprite)
+			set_visible(false)
+			gone = true
